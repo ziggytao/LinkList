61fe808f36031669904c5f26766b8121fbdc5b62
Tao Yue <ziggytao@163.com>
2017-04-12 23:00:58 +0800

    Update main.c
    For testing the functions declared before.

--- main.c
+++ main.c
@@ -10,7 +10,6 @@
 #include "list.h"
 #include "node.h"
 int main(int argc, const char * argv[]) {
-    // insert code here...
     List L;
     Init(&L);
     Push_front(&L, 55);
@@ -23,13 +22,13 @@ int main(int argc, const char * argv[]) {
     display_list(Begin(&L),End(&L));
     //Exchange(Begin(&L), Get_next(Begin(&L)));
     //display_list(Begin(&L),End(&L));
-   // Selection(&L);
-   // Selection2(&L);
-   // display_list(Begin(&L),End(&L));
+    //Selection(&L);
+    //Selection2(&L);
+    //display_list(Begin(&L),End(&L));
     //Bubble(&L);
     //display_list(Begin(&L),End(&L));
     //Exchange(Get_prev(End(&L)),Get_next(Begin(&L)));
-  //  display_list(Begin(&L),End(&L));
+    //display_list(Begin(&L),End(&L));
     Free(&L);
     return 0;
 }
